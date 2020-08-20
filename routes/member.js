db1c5a10b53ee74e979f430531c9d413fd8a721e
Tundeh <jamiu4freedom@gmail.com>
2020-08-20 12:34:27 +0100

    member routes

---
+++ routes/member.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const memberCtrl = require("../controllers/member");
+
+router.get("/list", memberCtrl.getAllMembers);
+router.post("/add", memberCtrl.addMember);
+module.exports = router;
