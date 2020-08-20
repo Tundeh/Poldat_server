85304435d842e8aa97e44c15e6b02787486ec501
Tundeh <jamiu4freedom@gmail.com>
2020-08-20 16:59:18 +0100

    include unique group id to the group schema

--- models/group.js
+++ models/group.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const groupSchema = new mongoose.Schema({
+  group_id: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
   leader: { type: String, required: true, unique: true },
   num_of_members: { type: Number, required: true, unique: true },
