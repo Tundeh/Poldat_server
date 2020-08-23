9cfba30b4247c8c01753711e7a0850ee56786833
Tundeh <jamiu4freedom@gmail.com>
2020-08-23 14:21:49 +0100

    add retun object in user validation

---
+++ validation/user.js
@@ -0,0 +1,38 @@
+const validator = require("validator");
+const isEmpty = require("./isEmpty");
+
+const validateUserInput = (user) => {
+  let errors = {};
+
+  // isEmpty validation loop
+  let userkeys = Object.keys(user);
+  for (i = 0; i < userkeys.length; i++) {
+    user[userkeys[i]] = !isEmpty(user[userkeys[i]]) ? user[userkeys[i]] : "";
+  }
+
+  if (!validator.isEmail(user.email)) {
+    errors.email = "email is invalid";
+  }
+  if (validator.isEmpty(user.password) || user.password.length < 5) {
+    errors.password = "password is invalid";
+  }
+  if (
+    validator.isEmpty(user.user_type) ||
+    !(user.user_type == "admin" || user.user_type == "employee")
+  ) {
+    errors.user_type = "user type is invalid";
+  }
+  if (validator.isEmpty(user.first_name)) {
+    errors.first_name = "first name is invalid";
+  }
+  if (validator.isEmpty(user.last_name)) {
+    errors.last_name = "last name is invalid";
+  }
+
+  return {
+    errors,
+    isValid: errors.length < 1,
+  };
+};
+
+module.exports = validateUserInput;
