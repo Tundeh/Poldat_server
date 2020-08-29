4f6b3ea0c4055894c9f9493739f7ac3cdbccb032
Tundeh <jamiu4freedom@gmail.com>
2020-08-29 14:44:55 +0100

    add users routes

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const userCtrl = require("../controllers/user");
+
+// @route   Post user/login
+// @desc    Post user
+// @access  Public
+router.post("/login", userCtrl.loginUser);
+
+// @route   GET user/list
+// @desc    Get users
+// @access  Private
+router.get("/list", userCtrl.getUsers);
+
+// @route   POST User
+// @desc    Post Users
+// @access  Private
+router.post("/add", userCtrl.addUser);
+
+// @route   DELETE api/user
+// @desc    Delete users
+// @access  Private
+router.delete("/:user_id", userCtrl.deleteUser);
+
+// @route   PUT api/user
+// @desc    Put user
+// @access  private
+router.put("/:user_id", userCtrl.updateUser);
+
+module.exports = router;
