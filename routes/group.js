8e50add8668d90964e634d425bf1b70c94b73749
Tundeh <jamiu4freedom@gmail.com>
2020-10-04 21:51:20 +0100

    Add groups routes

---
+++ routes/group.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const groupCtrl = require('../controllers/group.js');
+const group = require('../models/group.js');
+
+router.get('/');
+module.exports = router;
