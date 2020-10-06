904bdd3df590e8ee3daf65a4d017ae884947d1f7
Tundeh <jamiu4freedom@gmail.com>
2020-10-06 19:16:53 +0100

    validate group input in group controller

--- controllers/group.js
+++ controllers/group.js
@@ -1,29 +1,41 @@
-import Group from '../models/group';
+const Group = require('../models/group');
+const groupValidator = require('../Validation/group');
 
-module.exports.getGroup = (req, res, next) => {
+module.exports.getGroups = (req, res, next) => {
   Group.find()
     .then((groups) => res.status(200).json(groups))
-    .catch(res.status(400).json({ error: 'request unsuccessful' }));
+    .catch((err) => res.status(400).json({ error: 'request unsuccessful' }));
 };
 
 module.exports.createGroup = (req, res, next) => {
+  try {
+    const { errors, isValid } = groupValidator(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+  } catch {
+    const error = { message: 'invalid request body' };
+    return res.status(400).json({ error: error });
+  }
+
   const group = new Group({
-    group_id: req.body.id,
     name: req.body.name,
-    leader: req.body.leader,
+    leader: req.body.leader_id,
   });
 
   group
     .save()
-    .then(res.status(200).json({ message: 'group successfully saved' }))
-    .catch((error) => {
+    .then((err, doc) => {
+      res.status(200).json({ message: 'group successfully saved' });
+    })
+    .catch((err) => {
       res.status(500).json({ error: 'request was unsuccessful' });
     });
 };
 
 module.exports.updateGroup = (req, res, next) => {
   const group = new Group({
-    group_id: req.body.id,
     name: req.body.name,
     leader: req.body.leader,
   });
