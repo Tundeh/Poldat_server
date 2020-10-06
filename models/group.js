03a09e3801ec93d6f446820dcd0a00285f1c19b5
Tundeh <jamiu4freedom@gmail.com>
2020-10-06 19:26:42 +0100

    remove group_id and num_0f_members from group model and change leader to leader_id

--- models/group.js
+++ models/group.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 //Create Schema
 const groupSchema = new mongoose.Schema({
-  group_id: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
-  leader: { type: String, required: true, unique: true },
-  num_of_members: { type: Number, required: true, unique: true },
+  leader_id: { type: String },
 });
 
 groupSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model("Group", groupModel);
+module.exports = mongoose.model('Group', groupSchema);
