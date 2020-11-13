24dff32f32d01971faf055af0d7c0235aad3185b
Tundeh <jamiu4freedom@gmail.com>
2020-11-13 16:33:28 +0100

    add sms routes

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const mongoURI = require("./config/keys").mongoURI;
-const memberRoutes = require("./routes/member");
-const userRoutes = require("./routes/user");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const mongoURI = require('./config/keys').mongoURI;
+const memberRoutes = require('./routes/member');
+const userRoutes = require('./routes/user');
+const analysisRoutes = require('./routes/analysis');
+const authRoutes = require("./routes/auth");
+const groupRoutes = require('./routes/group');
+const smsRoutes = require('./routes/sms');
 const app = express();
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("mongodb successfully connected");
+    console.log('mongodb successfully connected');
   })
   .catch((error) => {
     console.log(error);
   });
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content, Accept, Content-Type, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content, Accept, Content-Type, Authorization'
   );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
-require("./middlewares/auth")(passport);
+require('./middlewares/auth')(passport);
 
-app.use("/api/member", memberRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use('/api/member', memberRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/groups', groupRoutes);
+app.use('/api/sms', smsRoutes);
+app.use('/api/analysis', analysisRoutes);
 
 module.exports = app;
